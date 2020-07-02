581ed2f7e1402d233a938334dd5824a69d7a509f
shani__tripathi <shani.tripathi01@gmail.com>
2020-07-02 12:42:19 +0100

    design made

---
+++ Tic-Tac-Toe/assets/js/main.js
@@ -0,0 +1,7 @@
+let a = document.getElementById("shani").innerText;
+// console.log(a);
+b = document.getElementsByClassName("temp");
+
+for (let i = 0; i < b.length; i++) {
+  b[i].innerText = "hello shani";
+}

b2c70f441e1c1b4b955fecb48e3809c292f395bb
shani__tripathi <shani.tripathi01@gmail.com>
2020-07-03 18:49:42 +0100

    Logic Implemented

--- Tic-Tac-Toe/assets/js/main.js
+++ Tic-Tac-Toe/assets/js/main.js
@@ -1,7 +1,86 @@
-let a = document.getElementById("shani").innerText;
-// console.log(a);
-b = document.getElementsByClassName("temp");
+var list = ["x", "o", "x", "o", "x", "o", "x", "o", "x"];
+function marker(id) {
+  console.log(list);
+  document.getElementById(id).innerText = list[list.length - 1];
+  document.getElementById(id).style.cursor = "not-allowed";
 
-for (let i = 0; i < b.length; i++) {
-  b[i].innerText = "hello shani";
+  list.pop();
+}
+
+function winCheck() {
+  if (
+    (document.getElementById(1).innerText == "x" &&
+      document.getElementById(2).innerText == "x" &&
+      document.getElementById(3).innerText == "x") ||
+    (document.getElementById(1).innerText == "x" &&
+      document.getElementById(4).innerText == "x" &&
+      document.getElementById(7).innerText == "x") ||
+    (document.getElementById(1).innerText == "x" &&
+      document.getElementById(5).innerText == "x" &&
+      document.getElementById(9).innerText == "x") ||
+    (document.getElementById(2).innerText == "x" &&
+      document.getElementById(5).innerText == "x" &&
+      document.getElementById(8).innerText == "x") ||
+    (document.getElementById(3).innerText == "x" &&
+      document.getElementById(6).innerText == "x" &&
+      document.getElementById(9).innerText == "x") ||
+    (document.getElementById(4).innerText == "x" &&
+      document.getElementById(5).innerText == "x" &&
+      document.getElementById(6).innerText == "x") ||
+    (document.getElementById(7).innerText == "x" &&
+      document.getElementById(8).innerText == "x" &&
+      document.getElementById(9).innerText == "x") ||
+    (document.getElementById(7).innerText == "x" &&
+      document.getElementById(5).innerText == "x" &&
+      document.getElementById(3).innerText == "x")
+  ) {
+    document.getElementById("wintext").innerText = "Player X Wins";
+    document.getElementById("restartwindow").style.display = "flex";
+  } else if (
+    (document.getElementById(1).innerText == "o" &&
+      document.getElementById(2).innerText == "o" &&
+      document.getElementById(3).innerText == "o") ||
+    (document.getElementById(1).innerText == "o" &&
+      document.getElementById(4).innerText == "o" &&
+      document.getElementById(7).innerText == "o") ||
+    (document.getElementById(1).innerText == "o" &&
+      document.getElementById(5).innerText == "o" &&
+      document.getElementById(9).innerText == "o") ||
+    (document.getElementById(2).innerText == "o" &&
+      document.getElementById(5).innerText == "o" &&
+      document.getElementById(8).innerText == "o") ||
+    (document.getElementById(3).innerText == "o" &&
+      document.getElementById(6).innerText == "o" &&
+      document.getElementById(9).innerText == "o") ||
+    (document.getElementById(4).innerText == "o" &&
+      document.getElementById(5).innerText == "o" &&
+      document.getElementById(6).innerText == "o") ||
+    (document.getElementById(7).innerText == "o" &&
+      document.getElementById(8).innerText == "o" &&
+      document.getElementById(9).innerText == "o") ||
+    (document.getElementById(7).innerText == "o" &&
+      document.getElementById(5).innerText == "o" &&
+      document.getElementById(3).innerText == "o")
+  ) {
+    document.getElementById("wintext").innerText = "Player O Wins";
+    document.getElementById("restartwindow").style.display = "flex";
+  } else {
+    if (list.length == 0) {
+      console.log("It's a draw");
+    }
+  }
+}
+
+function game(id) {
+  if (
+    document.getElementById(id).innerText != "x" &&
+    document.getElementById(id).innerText != "o"
+  ) {
+    marker(id);
+    winCheck();
+  }
+}
+
+function reload() {
+  location.reload();
 }
